64aa358f8ade4d825c9a941ed0875c754feb2215
benjamingoldenlineage <80452692+benjamingoldenlineage@users.noreply.github.com>
2021-06-20 10:36:37 +0300

    initial commit

--- aws/class-notes/galip.py
+++
@@ -1,2 +0,0 @@
-print("hello")
-pip intall flask
--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -14,5 +14,5 @@ def forth(id):
     return f'Id number of this page is {id}'
 if __name__ == '__main__' :
     
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
